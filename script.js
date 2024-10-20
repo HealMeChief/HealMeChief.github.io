0d0d3c29089cb193581c7bc997f476c6adcc8021
HealMeChief <az464608@gmail.com>
2024-10-20 22:57:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ const video = document.getElementById('customVideo');
 
 // Скрываем контролы по умолчанию
 video.removeAttribute('controls');
+
+const videos = document.querySelectorAll('video');
+
+// Применяем обработчики событий для каждого видео
+videos.forEach((video) => {
+    // Скрываем контролы по умолчанию
+    video.removeAttribute('controls');
+});
